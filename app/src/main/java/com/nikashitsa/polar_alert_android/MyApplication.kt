4bcd4ea1bde5c8d3a1c68b312f031ff4d36ab13d
nikashitsa <vernik91@gmail.com>
2026-09-01 22:35:59 +0200

    chore: remove extra comment

--- app/src/main/java/com/nikashitsa/polar_alert_android/MyApplication.kt
+++ app/src/main/java/com/nikashitsa/polar_alert_android/MyApplication.kt
@@ -7,8 +7,5 @@ import javax.inject.Inject
 
 @HiltAndroidApp
 class MyApplication: Application() {
-
-    // Injected purely so it is constructed at launch. Its init block restores a purchase this
-    // install may have missed and retries any acknowledgement that never reached Play.
     @Inject lateinit var billing: BillingRepository
 }
